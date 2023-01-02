bfe48885549559d7020ab88e074da074cbca11bd
Kisukeyas <kisuke.yas@gmail.com>
2023-01-02 19:00:00 +0900

    fix: index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,12 @@ import { useAuth0 } from "@auth0/auth0-react"
 
 
 export default function Home() {
-  const {loginWithRedirect} = useAuth0()
+  const {loginWithRedirect, logout} = useAuth0()
   return (
     <>
     <p>ログインする</p>
     <button onClick={() => loginWithRedirect()}>ログインする</button>
+    <button onClick={() => logout()}>ログアウトする</button>
     </>
   )
 }

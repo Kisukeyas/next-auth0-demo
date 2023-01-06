f53700afc8fc67b982275e24c7cdb09f06d0a79d
Kisukeyas <kisuke.yas@gmail.com>
2023-01-07 00:12:20 +0900

    fix: auth0 config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,11 @@ import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Auth0Provider domain="" clientId="" redirectUri="">
+    <Auth0Provider
+      domain={process.env.NEXT_PUBLIC_AUTH0_DOMAIN as string}
+      clientId={process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID as string}
+      redirectUri={process.env.NEXT_PUBLIC_AUTH0_REDIRECT_URI}
+    >
       <Component {...pageProps} />
     </Auth0Provider>
   );
